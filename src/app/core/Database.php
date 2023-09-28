cbe824edd5af11883d3f6feed20ca230d7648bba
SaddamAnnais <saddamannaiss@gmail.com>
2023-09-28 13:23:13 +0700

    feat: database scheme

--- src/app/core/Database.php
+++ src/app/core/Database.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . '/../../config/config.php';
 
-class DB {
+class DB
+{
     private $conn;
     private $stmt;
 
@@ -17,30 +18,44 @@ class DB {
             $this->conn = new PDO($dsn, DB_USER, DB_PASSWORD, $option);
 
             if ($this->conn) {
-            echo "Connected to the database successfully!";
+                echo "Connected to the database successfully!";
             }
         } catch (PDOException $e) {
             die($e->getMessage());
         }
 
         $create_user = "CREATE TABLE IF NOT EXISTS user (
-            user_id             INTEGER     PRIMARY KEY     AUTO_INCREMENT
-            -- TODO: Continue... (jangan lupa displit koma)
+            user_id         INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            username        VARCHAR(255) NOT NULL,
+            name            VARCHAR(255) NOT NULL,
+            password_hash   VARCHAR(255) NOT NULL,
+            is_admin        BOOLEAN DEFAULT FALSE
         )";
 
         $create_recipe = "CREATE TABLE IF NOT EXISTS recipe (
-            recipe_id           INTEGER     PRIMARY KEY     AUTO_INCREMENT
-            -- TODO: Continue... (jangan lupa displit koma)
+            recipe_id       INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            title           VARCHAR(255) NOT NULL,
+            `desc`          TEXT NOT NULL,
+            tag             VARCHAR(255) NOT NULL,
+            video_path      VARCHAR(255) NOT NULL,
+            image_path      VARCHAR(255) NOT NULL,
+            created_at      TIMESTAMP NOT NULL
         )";
 
         $create_playlist = "CREATE TABLE IF NOT EXISTS playlist (
-            playlist_id         INTEGER     PRIMARY KEY     AUTO_INCREMENT
-            -- TODO: Continue... (jangan lupa displit koma)
+            playlist_id     INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            title           VARCHAR(255) NOT NULL,
+            user_id         INT UNSIGNED,
+            created_at      DATE DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES user(user_id)
         )";
 
         $create_playlist_recipe = "CREATE TABLE IF NOT EXISTS playlist_recipe (
-            playlist_recipe_id  INTEGER     PRIMARY KEY     AUTO_INCREMENT
-            -- TODO: Continue... (jangan lupa displit koma)
+            recipe_id       INT UNSIGNED,
+            playlist_id     INT UNSIGNED,
+            PRIMARY KEY (recipe_id, playlist_id),
+            FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id),
+            FOREIGN KEY (playlist_id) REFERENCES playlist(playlist_id)
         )";
 
         try {
@@ -77,11 +92,13 @@ class DB {
         $this->stmt->bindValue($param, $value, $type);
     }
 
-    public function query($query) {
+    public function query($query)
+    {
         $this->stmt = $this->conn->prepare($query);
     }
 
-    public function exec() {
+    public function exec()
+    {
         $this->stmt->execute();
     }
 }
