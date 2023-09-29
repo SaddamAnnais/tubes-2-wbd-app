ef70473e24950fac40fb51757fcef43c0a304ef2
SaddamAnnais <saddamannaiss@gmail.com>
2023-09-29 17:30:23 +0700

    feat: CRUD for model user

--- src/app/model/User.php
+++ src/app/model/User.php
@@ -10,5 +10,51 @@ class UserModel {
         $this->db = new DB();
     }
 
-    // TODO: Implement CRUD
+    public function getUserByUsername($username)
+    {
+      $this->db->query('SELECT * FROM user WHERE username = :username');
+      $this->db->bind('username', $username);
+      return $this->db->fetch();
+    }
+
+    public function register($data)
+    {
+      $query = "INSERT INTO user (username, name, password_hash, is_admin) values (:username, :name, :password_hash, false)";
+      $this->db->query($query);
+      $this->db->bind('username', $data['username']);
+      $this->db->bind('name', $data['name']);
+      $this->db->bind('password_hash', password_hash($data['password'], PASSWORD_DEFAULT));
+      $this->db->exec();
+      return $this->db->stmt->rowCount();
+    }
+
+    public function updateUserById($userId, $data)
+    {
+        $query = "UPDATE user SET username = :username, name = :name WHERE user_id = :user_id";
+        $this->db->query($query);
+        $this->db->bind('username', $data['username']);
+        $this->db->bind('name', $data['name']);
+        $this->db->bind('user_id', $userId);
+        $this->db->exec();
+        return $this->db->stmt->rowCount();
+    }
+
+    public function updateUserPasswordById($userId, $newPassword)
+    {
+        $query = "UPDATE user SET password_hash = :password_hash WHERE user_id = :user_id";
+        $this->db->query($query);
+        $this->db->bind('password_hash', password_hash($newPassword, PASSWORD_DEFAULT));
+        $this->db->bind('user_id', $userId);
+        $this->db->exec();
+        return $this->db->stmt->rowCount();
+    }
+    
+    public function deleteUserById($userId)
+    {
+        $query = "DELETE FROM user WHERE user_id = :user_id";
+        $this->db->query($query);
+        $this->db->bind('user_id', $userId);
+        $this->db->exec();
+        return $this->db->stmt->rowCount();
+    }
 }
