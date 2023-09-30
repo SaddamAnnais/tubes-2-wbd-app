73937cceca6e6f2eee8505d1db38f86140fc3085
fakihap <fakih.anugerah@gmail.com>
2023-09-30 09:19:42 +0700

    feat: skeletoning playlist model

--- src/app/model/Playlist.php
+++ src/app/model/Playlist.php
@@ -10,5 +10,41 @@ class PlaylistModel {
         $this->db = new DB();
     }
 
-    // TODO: Implement CRUD
+    public function createPlaylist($data) {
+        $query = "";
+    }
+
+    public function getPlaylistsByTitle($title) {
+
+    }
+
+    public function getPlaylistsByOwner($username) {
+
+    }
+
+    public function getPlaylistOwner($playlistId) {
+
+    }
+
+    public function renamePlaylist($playlistId, $newTitle) {
+
+    }
+
+    public function deletePlaylist($playlistId) {
+
+    }
+
+    public function fetchAllRecipe($playlistId) {
+        
+    }
+
+    public function addRecipe($playlistId, $recipeId) {
+
+    }
+
+    public function removeRecipe($playlistId, $recipeId) {
+
+    }
+
+
 }
