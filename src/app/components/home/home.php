014c7d8abc2282352d51b52672363692e73afb00
fakihap <fakih.anugerah@gmail.com>
2023-10-01 22:51:37 +0700

    chore: init routing and components building

---
+++ src/app/components/home/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<?php
+    require_once '../templates/navbar.php';
+?>
+
+<head>
+    <title>Cooklyst!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" type="text/css" href="/src/public/styles/styles.css">
+</head>
+
+<body>
+    asu
+    <?php navbar() ?>
+    <div>
+        <!-- sidebar -->
+        
+        <div id="card-container">
+            mewo
+        </div>
+    </div>
+</body>
---
+++ src/app/components/templates/navbar.php
@@ -0,0 +1,14 @@
+<?php
+    function navbar() {
+?>
+        <nav>
+            <nav-wrapper>
+                hellow!
+            </nav-wrapper>
+            <div id="background">
+                taindaypo
+            </div>
+        </nav>
+<?php
+    } 
+?>
