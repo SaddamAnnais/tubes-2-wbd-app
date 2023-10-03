211a2bce459d60b6f8ceb47ff75b011321d7ca65
SaddamAnnais <saddamannaiss@gmail.com>
2023-10-03 12:16:21 +0700

    fix: import in controller

--- src/app/core/Controller.php
+++ src/app/core/Controller.php
@@ -4,19 +4,19 @@ class Controller
 {
     public function view($folder, $view, $data = [])
     {
-        require_once '../views/' . $folder . '/' . $view . '.php';
+        require_once __DIR__ . '/../views/' . $folder . '/' . $view . '.php';
         return new $view($data);
     }
 
     public function model($model)
     {
-        require_once '../models/' . $model . '.php';
+        require_once __DIR__ . '/../models/' . $model . '.php';
         return new $model();
     }
 
     public function middleware($middleware)
     {
-        require_once  '../middlewares/' . $middleware . '.php';
+        require_once __DIR__ . '/../middlewares/' . $middleware . '.php';
         return new $middleware();
     }
 }
