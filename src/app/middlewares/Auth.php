1cf6d55efc972a659d7d40d282ab3be8dd3bf1f4
SaddamAnnais <saddamannaiss@gmail.com>
2023-10-03 22:24:10 +0700

    feat: middleware for auth

---
+++ src/app/middlewares/Auth.php
@@ -0,0 +1,38 @@
+<?php
+
+class Auth
+{
+  private $db;
+
+  public function __construct()
+  {
+    $this->db = new DB();
+  }
+
+  public function isAuthenticated()
+  {
+    if (!isset($_SESSION['user_id'])) {
+      throw new DisplayedException('Unauthorized', 401);
+    }
+
+    $this->db->query('SELECT user_id FROM user WHERE user_id = :user_id LIMIT 1');
+    $this->db->bind('user_id', $_SESSION['user_id']);
+
+    $result = $this->db->fetch();
+
+    if (!$result) {
+      throw new DisplayedException('Unauthorized', 401);
+    }
+
+    return $result;
+  }
+
+  public function isAdmin()
+  {
+    $result = $this->isAuthenticated();
+
+    if ($result->is_admin) {
+      throw new DisplayedException('Unauthorized', 401);
+    }
+  }
+}
