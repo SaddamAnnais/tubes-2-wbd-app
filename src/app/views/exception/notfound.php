1074e0739ea49ca8647d8966d17b9d829044fbff
SaddamAnnais <saddamannaiss@gmail.com>
2023-10-04 12:33:18 +0700

    chore: import and 404 page skeleton

---
+++ src/app/views/exception/notfound.php
@@ -0,0 +1,14 @@
+<?php
+
+class notfound implements ViewInterface
+{
+    public function __construct($data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function render()
+    {
+        require_once __DIR__ . '/../../components/exceptions/404.php';
+    }
+}
