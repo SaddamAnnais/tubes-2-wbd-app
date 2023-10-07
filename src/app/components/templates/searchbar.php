8c1bf67e7e558217d8e1a0dbfbec4e0f02f02a59
fakihap <fakih.anugerah@gmail.com>
2023-10-07 18:30:05 +0700

    fix: remove spellcheck from search bar

--- src/app/components/templates/searchbar.php
+++ src/app/components/templates/searchbar.php
@@ -4,7 +4,7 @@
 ?>
         <div id="searchbar">
             <form id="searchform" action="/home" method="get">
-                <input type="text" id="searchtext" placeholder="Cari resep ..." />
+                <input type="text" id="searchtext" placeholder="Cari resep ..." spellcheck="false" />
                 <button type="submit" id="searchsubmit"> 
                     <img id="searchsubmit_icon" src="<?php echo BASE_URL ?>/static/icon/search.svg" alt="search"/>
                 </button>
