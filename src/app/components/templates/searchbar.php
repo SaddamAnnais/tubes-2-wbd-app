202188cf3ada9c9b9bed3d7a9c0d320e8bee630c
fakihap <fakih.anugerah@gmail.com>
2023-10-07 16:46:33 +0700

    feat: searchbar input
    - not yet wired
    - debouncing not yet decided
    - home get method not yet decided

--- src/app/components/templates/searchbar.php
+++ src/app/components/templates/searchbar.php
@@ -3,7 +3,12 @@
 
 ?>
         <div id="searchbar">
-            
+            <form id="searchform" action="/home" method="get">
+                <input type="text" id="searchtext" placeholder="Cari resep ..." />
+                <button type="submit" id="searchsubmit"> 
+                    <img id="searchsubmit_icon" src="<?php echo BASE_URL ?>/static/icon/search.svg" alt="search"/>
+                </button>
+            </form>
         </div>
 <?php
     }
