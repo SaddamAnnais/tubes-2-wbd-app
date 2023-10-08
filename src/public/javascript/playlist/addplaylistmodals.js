35939d1b731e1d78c19f1dd12da9df84798b5838
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2023-10-08 15:22:46 +0700

    feat: my playlists page (get and post)

---
+++ src/public/javascript/playlist/addplaylistmodals.js
@@ -0,0 +1,70 @@
+const form = document.querySelector("#modal-form");
+const res = document.querySelector("#result-alert");
+
+const title_input = document.querySelector("#modal-input");
+const title_alert = document.querySelector("#playlist-alert");
+let title_validate = false;
+
+var addModal = document.getElementById("add-modal");
+var addOpenBtn = document.getElementById("add-button");
+var addXBtn = document.getElementById("close-add");
+var addCloseBtn = document.getElementById("cancel-add");
+
+// open modal
+addOpenBtn.onclick = function () {
+  addModal.style.display = "flex";
+};
+
+// close modal
+addXBtn.onclick = function () {
+  addModal.style.display = "none";
+};
+addCloseBtn.onclick = function () {
+  addModal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == addModal) {
+    addModal.style.display = "none";
+  }
+};
+
+form &&
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const title_value = title_input.value.trim();
+
+    if (!title_value) {
+      title_alert.innerText = "Playlist title cannot be empty.";
+      title_alert.className = "alert shown-error";
+      title_validate = false;
+    } else {
+      title_alert.innerText = "";
+      title_alert.className = "alert hidden";
+      title_validate = true;
+    }
+    console.log(title_value);
+    if (!title_validate) {
+      res.className = "alert hidden";
+      res.innerText = "";
+      return;
+    }
+
+    const xhr = new XMLHttpRequest();
+
+    const data = new FormData();
+    data.append("title", title_value);
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState === XMLHttpRequest.DONE) {
+        console.log(this.status);
+        if (this.status === 201) {
+          const payload = JSON.parse(this.responseText);
+          location.replace(payload.url);
+        }
+      }
+    };
+
+    xhr.open("POST", `/public/playlist/`, true);
+    xhr.send(data);
+  });
+
