a4a279eb83a6e5b8ba22d34a0239fbd18fd4d651
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2023-10-08 15:22:09 +0700

    feat: playlists get and post controller

---
+++ src/app/views/playlist/PlaylistsView.php
@@ -0,0 +1,15 @@
+<?php
+
+class PlaylistsView implements ViewInterface
+{
+    public $data;
+    public function __construct($data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function render()
+    {
+        require_once __DIR__ . '/../../components/playlist/playlists.php';
+    }
+}
